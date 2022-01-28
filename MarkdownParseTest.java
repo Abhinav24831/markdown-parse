80ddf5117b78e5eb04c727f2569070e0197dd678
Abhinav24831 <achandra@ucsd.edu>
2022-01-27 19:40:44 -0800

    Finished writing tests for all 4 test files and debugging for test file 3 by adding if statements after each var in while loop

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,60 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void links() throws IOException{
+        ArrayList<String> output = new ArrayList<>();
+        Path fileName = Path.of("test-file.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        output.add("https://something.com");
+        output.add("some-page.html");
+        
+        assertEquals(output, links);
+    }
+
+    @Test
+    public void links2() throws IOException{
+        ArrayList<String> output = new ArrayList<>();
+        Path fileName = Path.of("test-file2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        output.add("https://something.com");
+                
+        assertEquals(output, links);
+    }
+
+    @Test
+    public void links3() throws IOException{
+        ArrayList<String> output = new ArrayList<>();
+        Path fileName = Path.of("test-file3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        output.add("https://something.com");
+        
+        assertEquals(output, links);
+    }
+
+    @Test
+    public void links4() throws IOException{
+        ArrayList<String> output = new ArrayList<>();
+        Path fileName = Path.of("test-file3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        output.add("https://something.com");
+        
+        assertEquals(output, links);
+    }
+
+
 }
