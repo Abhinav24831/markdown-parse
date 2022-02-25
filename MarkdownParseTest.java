bf352b04db993e71bc0d81a1001c48a5a46f9244
Abhinav Chandra <cs15lwi22avb@ieng6-201.ucsd.edu>
2022-02-25 01:40:06 -0800

    added tests and files for lab report 4

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -56,5 +56,44 @@ public class MarkdownParseTest {
         assertEquals(output, links);
     }
 
+    @Test
+    public void testSnippet1() throws IOException{
+        ArrayList<String> output = new ArrayList<>();
+        Path fileName = Path.of("test-snippet-1.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        output.add("`google.com");
+        output.add("google.com");
+        output.add("ucsd.edu");
+
+        assertEquals(output, links);
+    }
+
+    @Test
+    public void testSnippet2() throws IOException{
+        ArrayList<String> output = new ArrayList<>();
+        Path fileName = Path.of("test-snippet-2.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        output.add("a.com");
+        output.add("a.com(())");
+        output.add("example.com");
+
+        assertEquals(output, links);
+    }
+
+    @Test
+    public void testSnippet3() throws IOException{
+        ArrayList<String> output = new ArrayList<>();
+        Path fileName = Path.of("test-snippet-3.md");
+	    String contents = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(contents);
+        output.add("https://www.twitter.com");
+        output.add("https://ucsd-cse15l-w22.github.io/");
+        output.add("https://cse.ucsd.edu/");
+
+        assertEquals(output, links);
+    }
+
 
 }
